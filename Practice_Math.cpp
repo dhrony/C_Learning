e938668533dd4d5330ba851f7242685edeeb52cd
dhrony <>
2024-04-06 10:07:29 -0400

    Completed Practice 01

---
+++ Practice_Math.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{
+    int num01 = 11;
+    int num02 = 5;
+
+    cout << "Result for Number Addition is = " << num01 + num02 << endl;
+    cout << "Result for Number Substraction is = " <<num01 - num02 << endl;
+    cout << "Result for Number Multiplication is = " <<num01 * num02 << endl;
+    return 0;
+
+}
