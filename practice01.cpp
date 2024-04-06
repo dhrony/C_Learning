a2572caea51ac6a158d5f3da4e1be24bb464af00
dhrony <>
2024-04-06 09:34:00 -0400

    First Code Commint for C++

---
+++ practice01.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+
+// Simple printing practice
+
+{
+    cout<< "Name: Md Dider Hossain Rony\n";
+/*
+This is a multple line comments
+This is second line of multi line comments
+*/
+    cout<< "Daughter's Name: Tazanna Roja\n";
+    cout<< "Daughters school: Crescent Town EE"<<endl;
+    cout<< "My University ID: 060105147"<<endl;
+    return 0;
+}
