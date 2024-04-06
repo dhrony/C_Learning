db8502743cd11c892f38fa63d64be95b18ed7b41
dhrony <>
2024-04-06 09:52:40 -0400

    Variable Class

---
+++ cpp_variable.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{
+    int num = 11;
+    float num1 = 10.55;
+    char character = 'C';
+    string comment = "Tazanna";
+    bool myBool = true;
+
+    int number01;
+    number01 = 15;
+    cout<<number01<<endl;
+
+    int sum = number01 + num;
+
+    cout << sum << endl;
+}
+
+// All C++ variables must be identified with unique names
